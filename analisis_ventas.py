5f053c29fc0c3b600221665d9930bcef7b5b2d14
Francisco José Sepúlveda Venegas <frsepulv.linux@gmail.com>
2024-12-01 16:46:25 -0300

    Generación de gráfico

--- analisis_ventas.py
+++ analisis_ventas.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
     data = pd.read_csv("ventas_producto.csv")
     data["Precio_total"] = data["Cantidad"] * data["Precio"]
+    plt.bar(data["Producto"], data["Precio_total"])
+    plt.xlabel("Producto")
+    plt.ylabel("Precio total")
+    plt.title("Total ventas por producto")
+    # Para pruebas
+    # plt.show()
+    plt.savefig("grafico.png")
